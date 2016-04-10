558d6add91e431f993eb0e95d4b10035e3d902d9
Shobhit Maheshwari <shobhitm.iit@gmail.com>
2016-04-10 16:11:39 -0700

    Changed datetime serialization

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,13 +1,33 @@
 from rest_framework import serializers
 from snippets.models import Snippet, Friendship
 from django.contrib.auth.models import User
+import time
+import datetime
+
+
+class UnixEpochDateField(serializers.DateTimeField):
+
+    def to_representation(self, value):
+        """ Return epoch time for a datetime object or ``None``"""
+        import time
+        try:
+            return int(time.mktime(value.timetuple()))
+        except (AttributeError, TypeError):
+            return None
+
+    def to_internal_value(self, value):
+        import datetime
+        return datetime.datetime.fromtimestamp(int(value))
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    starttime = UnixEpochDateField(source='created', required=False)
+    owner = serializers.ReadOnlyField(source='owner.username', required=False)
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'message', 'created', 'owner')
-        owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ('id', 'title', 'message', 'starttime', 'owner')
+
 
 
 class UserSerializer(serializers.ModelSerializer):
